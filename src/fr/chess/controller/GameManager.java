b68249fdd9a325960e1538284257014ad105b5a7
William TCHANG <william.tchang4@gmail.com>
2026-03-27 00:06:30 +0100

    feat(GamManager): implement the initial board game setUpBoard()

--- src/fr/chess/controller/GameManager.java
+++ src/fr/chess/controller/GameManager.java
@@ -1,9 +1,7 @@
 package fr.chess.controller;
 
-import fr.chess.model.Board;
-import fr.chess.model.Color;
-import fr.chess.model.Coordinate;
-import fr.chess.model.Piece;
+import fr.chess.model.*;
+import fr.chess.model.piece.*;
 import fr.chess.referee.MoveValidator;
 
 public class GameManager {
@@ -15,6 +13,7 @@ public class GameManager {
         this.board = new Board();
         this.currentTurn = Color.WHITE;
         this.validator = new MoveValidator();
+        setUpBoard();
     }
 
     /**
@@ -40,6 +39,51 @@ public class GameManager {
 
         switchTurn();
         return false;
+    }
+
+    /**
+     * Fill initial board.
+     */
+    private void setUpBoard(){
+        PieceType[] setup = {
+                PieceType.ROOK,PieceType.KNIGHT,
+                PieceType.BISHOP,PieceType.QUEEN,
+                PieceType.KING,PieceType.BISHOP,
+                PieceType.KNIGHT,PieceType.ROOK
+        };
+
+        for(int col = 0;col<8;col++){
+            // Black Pieces (row 0)
+            board.setPiece(createPiece(Color.BLACK,setup[col]),
+                    new Coordinate(0,col));
+            // Black Pawns (row 1)
+            board.setPiece(createPiece(Color.BLACK,PieceType.PAWN),
+                    new Coordinate(1,col));
+            // White Pawns (row 6)
+            board.setPiece(createPiece(Color.WHITE,PieceType.PAWN),
+                    new Coordinate(6,col));
+            // White Pieces (row 7)
+            board.setPiece(createPiece(Color.WHITE,setup[col]),
+                    new Coordinate(7,col));
+        }
+    }
+
+    /**
+     * Factory function to create the good sub class thanks to PieceType.
+     *
+     * @param color: the player color
+     * @param type: the piece type
+     * @return the good piece class.
+     */
+    private Piece createPiece(Color color,PieceType type){
+        return switch (type){
+            case ROOK -> new Rook(color);
+            case KNIGHT -> new Knight(color);
+            case BISHOP -> new Bishop(color);
+            case QUEEN -> new Queen(color);
+            case KING -> new King(color);
+            default -> throw new IllegalArgumentException("Unknow type");
+        };
     }
 
     /**
