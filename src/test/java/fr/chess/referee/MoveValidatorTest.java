669f54a3ed9b55931907580424bf57e6b941ea5e
William TCHANG <william.tchang4@gmail.com>
2026-03-29 22:43:24 +0200

    test(MoveValidator): legal move in work

--- src/test/java/fr/chess/referee/MoveValidatorTest.java
+++ src/test/java/fr/chess/referee/MoveValidatorTest.java
@@ -62,7 +62,26 @@ class MoveValidatorTest {
         assertEquals(expected, MoveValidator.isPathClear(source, target, board), message);
     }
 
-    @Test
-    void isLegalMove() {
+    @ParameterizedTest(name = "Test {index}: at {0} to {1} expected {2} ({3})")
+    @CsvSource({
+            // SOURCE, TARGET, EXPECTED, MESSAGE
+            "d2, d7, false, 'Block by pawn in d5'",
+            "d2, d4, true,  'Vertical path clear before pawn'",
+            "h1, b7, false, 'Block by Pawn in d5'",
+            "h1, f3, true,  'Diagonal path clear before pawn'",
+            "e4, d5, true,  'No path between source and target'",
+            "e4, e5, true,  'Pawn path clear'",
+            "c7, c5, false, 'Black pawn path block by white pawn'",
+            "b7, b5, true,  'Black pawn path clear'",
+
+    })
+    void isLegalMove(String src, String dest, boolean expected, String message) {
+        Board board = Utils.strToBoard(pathClearBoardTest);
+        MoveValidator validator = new MoveValidator();
+        board.displayBoard();
+        Coordinate source = Utils.strToCoords(src);
+        Coordinate target = Utils.strToCoords(dest);
+
+        assertEquals(expected, validator.isLegalMove(source, target, board), message);
     }
 }
